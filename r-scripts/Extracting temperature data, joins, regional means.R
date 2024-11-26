b4deddd00d67c97bcccd6ebc34122c369f4ecbcf
Stuart8924 <bulmers@uoguelph.ca>
2024-11-26 15:42:13 -0500

    Stuart

--- r-scripts/Extracting temperature data, joins, regional means.R
+++ r-scripts/Extracting temperature data, joins, regional means.R
@@ -123,29 +123,34 @@ lm_temp <- lm(mean_sst ~ year, temp)
 summary(lm_temp)
 
 #Plot yearly mean summer SST
-ggplot(mean_sst, aes(x = year,
+sum_sst <- ggplot(mean_sst, aes(x = year,
                      y = mean_sst,
                      group = region)) +
-  geom_point(aes(shape = region, colour = region)) +
+  geom_point(aes(shape = region, colour = region, size = 5)) +
   scale_colour_manual(values = c("Tropical" = "red",
                                  "Temperate" = "blue")) +
   geom_smooth(method = "lm") +
   geom_text(aes(x = 1990, y = 26, 
                 label = "y = 0.013803x + 0.276293, R² = 0.644, p < 0.05"), 
-            color = "red", size = 4) +
+            color = "red", size = 8) +
   geom_text(aes(x = 1990, y = 20, 
                 label = "y = 0.020160x - 23.054953, R² = 0.7326, p < 0.05"), 
-            color = "blue", size = 4) +
+            color = "blue", size = 8) +
   xlab("Year") +
   ylab("Mean SST (°C)") +
-  theme_minimal() +
-  theme(panel.grid.major = element_line(color = "gray80"),
+  theme_minimal(base_size = 14)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.grid.major = element_line(color = "gray80"),
         panel.grid.minor = element_blank(),
-        text = element_text(size = 12), 
-        axis.text = element_text(size = 10), 
-        axis.title = element_text(size = 12),      
-        legend.position = "none")
+        text = element_text(size = 30), 
+        axis.text = element_text(size = 15), 
+        axis.title = element_text(size = 30),      
+        legend.position = "right") +
+  guides(size = "none",
+         shape = guide_legend(override.aes = list(size = 5))) +
+  labs(colour = "Region", shape = "Region")
 
+ggsave("Figures/Mean summer SST.png", plot = sum_sst, bg = "transparent", width = 20, height = 10, dpi = 300)
 
 
 
