163817529233cae4ed4c17cc4af340eae9bfe85c
Lukasz Padiasek <kokafishin@gmail.com>
2024-12-03 13:55:29 -0500

    Edited comments in the code

--- r-scripts/rfishbase_biotime.R
+++ r-scripts/rfishbase_biotime.R
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-#
-# Synonym checking will need to make use of synonyms(species_list) from the rfishbase library
-#
 # Load necessary libraries
 library("rfishbase")
 library(readr)
@@ -14,59 +10,13 @@ biotime <- read.csv("data-processed/BioTime_processed.csv")
 biotime <- biotime %>%
   mutate(species_clean = gsub("_", " ", genus_species))
 
-# Create species_list from unique values in the species_clean column
+# Create species_list from unique values in biotime$species_clean
 species_list <- unique(biotime$species_clean)
 
-synonyms(species_list) 
-
-# Get table with species occurences
-ecoltbl <- ecology(species_list)
-
-# Filter species that are marine (Netric or Oceanic)
-marine_species <- ecoltbl %>%
-  filter(Neritic == -1 | Oceanic == -1) %>%
-  select(Species)
-
-# Keep only marine species in species_list
-species_list <- marine_species$Species
-
-# Create table of Tmax and Tmin for each species
-temptbl <- stocks(species_list, c("TempMin", "TempMax", "Species"))
-
-# Keep only the first occurrence for each Species
-temptbl <- temptbl %>%
-  distinct(Species, .keep_all = TRUE)
-
-# Join the temperature data with biotime and store the result in btfb_with_NA
-btfb_with_NA <- biotime %>%
-  left_join(temptbl, by = c("species_clean" = "Species"))
-
-# Filter out rows with NAs in TempMin or TempMax
-btfb_without_NA <- btfb_with_NA %>%
-  filter(!is.na(TempMin) & !is.na(TempMax))
-
-# Save new tables to CSVs
-write.csv(btfb_with_NA, "data-processed/btfb_with_NA.csv", row.names = FALSE)
-=======
-# Load necessary libraries
-library("rfishbase")
-library(readr)
-library(dplyr)
-
-# Load data
-biotime <- read.csv("data-processed/BioTime_processed.csv")
-
-# Clean species names in biotime to match format in rfishbase (remove underscores)
-biotime <- biotime %>%
-  mutate(species_clean = gsub("_", " ", genus_species))
-
-# Create species_list from unique values in the species_clean column
-species_list <- unique(biotime$species_clean)
-
-# Create table of all synonyms on FishBasefor names in BioTime
+# Create table of all synonyms on FishBase for names in BioTime
 specsynon <- synonyms(species_list)
 
-# Map BioTime names to FishBase names
+# Match BioTime names to FishBase accepted names
 synonym_mapping <- specsynon %>%
   group_by(synonym) %>%
   filter(if (any(Status == "accepted name", na.rm = TRUE)) Status == "accepted name" else Status == "synonym") %>%
@@ -78,18 +28,18 @@ synonym_mapping <- specsynon %>%
 # Get table with species occurences
 ecoltbl <- ecology(synonym_mapping$fishbase_name)
 
-# Filter species that are marine (Netric or Oceanic)
+# Filter for species that are marine (Neritic or Oceanic)
 marine_species <- ecoltbl %>%
   filter(Neritic == -1 | Oceanic == -1) %>%
   select(Species)
 
-# Keep only marine species in species_list
+# Update species_list to include only marine species
 species_list <- marine_species$Species
 
 # Create table of Tmax and Tmin for each species
 temptbl <- stocks(species_list, c("TempMin", "TempMax", "Species"))
 
-# Remove rows with NA in TempMin or TempMax, then keep the first occurrence if duplicates exist
+# Remove rows with NA values in temptbl$TempMin or temptbl$TempMax, then keep only the first occurrence if duplicates exist
 temptbl <- temptbl %>%
   filter(!is.na(TempMin) & !is.na(TempMax)) %>%
   distinct(Species, .keep_all = TRUE)  
@@ -102,17 +52,16 @@ synonym_mapping <- synonym_mapping %>%
 btfb_with_NA <- biotime %>%
   left_join(synonym_mapping, by = c("species_clean" = "biotime_name"))
 
-# Organize table
+# Organize & clean table
 btfb_with_NA <- btfb_with_NA %>%
   select(-species_clean) %>%
   rename(temp_min = TempMin, temp_max = TempMax) %>%
   select(abundance:genus_species, fishbase_name, everything())
 
-# Filter out rows with NAs in TempMin or TempMax
+# Filter out rows with NAs in temptbl$temp_min or temptbl$temp_max
 btfb_without_NA <- btfb_with_NA %>%
   filter(!is.na(temp_min) & !is.na(temp_max))
 
 # Save new tables to CSVs
 write.csv(btfb_with_NA, "data-processed/btfb_with_NA.csv", row.names = FALSE)
->>>>>>> f0aec0cc23e34b0cc904373b7c095e53f59937ce
 write.csv(btfb_without_NA, "data-processed/btfb_without_NA.csv", row.names = FALSE)
